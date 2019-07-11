bc49151e4514e4bfdc90d2f7b24264bdcff879bc
Roman <romankryk1803@gmail.com>
2019-07-12 00:25:49 +0300

    NewsObject

--- app/src/main/java/com/example/newsone2/domain/NewsObject.kt
+++ app/src/main/java/com/example/newsone2/domain/NewsObject.kt
@@ -1,6 +1,5 @@
 package com.example.newsone2.domain
 
 // дописать параметры
-class NewsObject(id: Int, url: String, title: String, descrip: String) {
-
-}
+class NewsObject(id: Int = 1, url: String = "", imageUrl: String = "", title: String = "", descrip: String = "",
+                 byline: String = "", copyright: String = "", date: String = "", source: String = "", favState: Boolean = false)
