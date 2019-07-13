a86486056717eefea66b1e7c0336b8dafe2e67d6
Roman <romankryk1803@gmail.com>
2019-07-13 14:26:06 +0300

    Load and Save Pages

--- app/src/main/java/com/example/newsone2/dao/WebPages.kt
+++ app/src/main/java/com/example/newsone2/dao/WebPages.kt
@@ -1,3 +1,27 @@
 package com.example.newsone2.dao
 
-class WebPages
+import android.graphics.Bitmap
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import java.io.File
+import java.io.File.separator
+
+private const val TAG = "WebPages"
+
+class WebPages(private val hashName: String, private val filesDir: File, private val hashDir: String): WebViewClient() {
+    override fun onPageStarted(
+        view: WebView, url: String,
+        favicon: Bitmap?
+    ) {}
+
+    override fun onPageFinished(view: WebView, url: String) {
+        super.onPageFinished(view, url)
+        Log.d(TAG, "onPageFinished: ")
+        view.saveWebArchive(
+            filesDir.absolutePath + separator + hashDir + separator + hashName
+        )
+    }
+
+    override fun onLoadResource(view: WebView, url: String) {}
+}
--- app/src/main/java/com/example/newsone2/ui/InnerActivity.kt
+++ app/src/main/java/com/example/newsone2/ui/InnerActivity.kt
@@ -1,17 +1,54 @@
 package com.example.newsone2.ui
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.newsone2.R
+import com.example.newsone2.dao.WebPages
+import kotlinx.android.synthetic.main.activity_inner.*
+import org.apache.commons.codec.binary.Hex
+import org.apache.commons.codec.digest.DigestUtils
+import java.io.File
+import java.io.File.separator
 
 private const val TAG = "InnerActivity"
+private const val hashDir = "webPages"
 
 class InnerActivity : AppCompatActivity() {
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_inner)
+        setContentView(R.layout.activity_inner)
         val url = intent.extras!!.getString("url")!!
         val favState = intent.extras!!.getBoolean("favState")
+
+        web_view.settings.javaScriptEnabled = true
+
+        if (favState) {
+            web_view.loadUrl(url)
+            return
+        }
+
+        val hash = DigestUtils.sha1(url)
+        val hashName = String(Hex.encodeHex(hash)) + ".mht"
+        hashPage(hashName, url)
+    }
+
+    private fun hashPage(hashName: String, url: String) {
+        val client = WebPages(hashName, filesDir, hashDir)
+        web_view.webViewClient = client
+        var file = File(filesDir.absolutePath + separator + hashDir)
+        file.mkdirs()
+
+        file = File(file, hashName)
+        if (file.exists()) {
+            Log.d(TAG, "onCreate: tryLoad $hashName")
+            web_view.loadUrl("file:///" + file.path)
+        } else {
+            Log.d(TAG, "onCreate: fail $hashName")
+            web_view.loadUrl(url)
+        }
     }
 }
